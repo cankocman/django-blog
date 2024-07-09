d4e04566c6dd575439c1f8b6b430f75af271f7fc
Can Koçman <cankocman@Cans-MacBook-Air-5.local>
2024-07-09 14:39:50 +0300

    url change

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import BlogPost, Comment
 
+
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date')
     search_fields = ('title', 'content')
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
